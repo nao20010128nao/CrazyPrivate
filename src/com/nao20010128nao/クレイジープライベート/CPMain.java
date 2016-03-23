56378e1b9254f3ec8949486a4cce4877200c0279
nao20010128nao <nao20010128@gmail.com>
2016-03-23 16:31:30 +0900

    追加

---
+++ src/com/nao20010128nao/クレイジープライベート/CPMain.java
@@ -0,0 +1,22 @@
+package com.nao20010128nao.クレイジープライベート;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class CPMain extends NanoHTTPD {
+
+	public CPMain(int port) {
+		super(port);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public CPMain(String hostname, int port) {
+		super(hostname, port);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
