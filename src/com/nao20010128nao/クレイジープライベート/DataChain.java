eb904961684403f33c54ae97adb7babe265ce0c7
nao20010128nao <nao20010128@gmail.com>
2016-06-30 15:10:41 +0900

    修正

--- src/com/nao20010128nao/クレイジープライベート/DataChain.java
+++ src/com/nao20010128nao/クレイジープライベート/DataChain.java
@@ -498,7 +498,7 @@ public class DataChain {
 									Calendar calendar = Calendar.getInstance();
 									calendar.setTimeInMillis(ers.currentMillis);
 									LocalDateTime ldt = LocalDateTime.of(calendar.get(Calendar.YEAR),
-											calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+											calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH),
 											calendar.get(Calendar.HOUR)
 													+ (calendar.get(Calendar.AM_PM) == Calendar.PM ? 12 : 0),
 											calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND));
@@ -534,7 +534,7 @@ public class DataChain {
 									Calendar calendar = Calendar.getInstance();
 									calendar.setTimeInMillis(ggs.currentMillis);
 									LocalDateTime ldt = LocalDateTime.of(calendar.get(Calendar.YEAR),
-											calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+											calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH),
 											calendar.get(Calendar.HOUR)
 													+ (calendar.get(Calendar.AM_PM) == Calendar.PM ? 12 : 0),
 											calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND));
